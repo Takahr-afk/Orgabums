015b798f836d565c8643960179d74389aec16bfb
Jannik Andres <ja075822@fh-muenster.de>
2025-09-22 15:48:26 +0200

    Added Login html

---
+++ src/main/java/com/jannikscode/orgabums/OrgabumsApplication.java
@@ -0,0 +1,13 @@
+package com.jannikscode.orgabums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgabumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrgabumsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jannikscode/orgabums/OrgabumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jannikscode.orgabums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgabumsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
