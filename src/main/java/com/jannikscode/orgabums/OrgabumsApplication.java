f4b83064d1a66b09f630a165337dcb996e64bfa4
Jannik Andres <ja075822@fh-muenster.de>
2025-09-23 14:54:37 +0200

    changed imports to h2 database & bugfix

--- src/main/java/com/jannikscode/orgabums/OrgabumsApplication.java
+++ src/main/java/com/jannikscode/orgabums/OrgabumsApplication.java
@@ -1,13 +1,32 @@
 package com.jannikscode.orgabums;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class OrgabumsApplication {
+@Log
+public class OrgabumsApplication implements CommandLineRunner {
+
+
+    private final DataSource dataSource;
+
+    public OrgabumsApplication(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OrgabumsApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("DataSource: "+ dataSource.toString());
+        final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+        restTemplate.execute("SELECT 1");
+    }
 }
