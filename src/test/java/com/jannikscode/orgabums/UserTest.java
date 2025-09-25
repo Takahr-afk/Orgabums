d76673ff547cc57ef1134e63af942a2b27186632
Jannik Andres <ja075822@fh-muenster.de>
2025-09-25 07:48:54 +0200

    UserClassTest added

---
+++ src/test/java/com/jannikscode/orgabums/UserTest.java
@@ -0,0 +1,32 @@
+package com.jannikscode.orgabums;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void addCase() {
+    }
+
+    @Test
+    void removeCase() {
+    }
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void getUser_Cases() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void setUser_Cases() {
+    }
+}
